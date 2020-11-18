87a392d8dcc9514c37305091c3d116f8944d1ff2
Hiram Montesillo <montesillo25@gmail.com>
2020-11-17 23:49:53 -0600

    Actualizar automaticamente

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BadgesList from '../components/BadgesList';
 import PageLoading from '../components/PageLoading.js';
 import PageError from '../components/PageError';
+import MiniLoader from '../components/MiniLoader';
 import api from '../api';
 
 
@@ -18,6 +19,11 @@ class Badges extends React.Component{
 
     componentDidMount(){
         this.fetchData()
+
+        this.intervalId = setInterval(this.fetchData, 5000);
+    }
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
     }
 
     fetchData = async () => {
@@ -32,7 +38,7 @@ class Badges extends React.Component{
     }
 
     render(){
-        if(this.state.loading === true){
+        if(this.state.loading === true && !this.state.data){
             return <PageLoading />;
         }
         if(this.state.error){
@@ -59,7 +65,7 @@ class Badges extends React.Component{
                 <div className="Badges__list">
                     <div className="Badges__container">
                         <BadgesList badges = {this.state.data} />
-                        
+                        {this.state.loading && <MiniLoader />}
                     </div>
                 </div>
             </React.Fragment>
