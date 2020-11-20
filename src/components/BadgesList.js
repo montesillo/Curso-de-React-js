f8726557d9fa45fac3c26fc5084ff68bf8338a39
Hiram Montesillo <montesillo25@gmail.com>
2020-11-20 00:46:44 -0600

    Portales y modales

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -22,7 +22,7 @@ class BadgesList extends React.Component{
                     {this.props.badges.map((badge) => {
                         return(
                             <li  key={badge.id}>
-                                <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}/edit`}>
+                                <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}`}>
                                     <div className="BadgesListItem">
                                         <Gravatar email={badge.email} className="BadgesListItem__avatar"/>
                                         <div className="BadgesData">
---
+++ src/pages/BadgeDetails.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import Badge from '../components/Badge'; 
+import confLogo from '../images/platziconf-logo.svg';
+import DeleteBadgeModal from '../components/DeleteBadgeModal';
+
+function BadgeDetails (props){
+    const badge = props.badge;
+    return(
+        <div>
+                <div className="BadgeDetails__hero">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
+                                <img src={confLogo} alt="Logo de la Conferencia" />
+                            </div>
+                            <div className="col-6 BadgeDetails__hero-attendant-name">
+                                <h1>{badge.firstName} {badge.lastName} </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <Badge 
+                            firstName={badge.firstName}
+                            lastName={badge.lastName}
+                            email={badge.email}
+                            twitter={badge.twitter}
+                            jobTitle={badge.jobTitle}
+                            />
+                        </div>
+                        <div className="col-6">
+                            <h2>Actions</h2>
+                            <div>
+                                <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`} >Edit</Link>
+                            </div>
+                            <div>
+                                <button onClick={props.onOpenModal}  className="btn btn-danger">Delete</button>
+                                <DeleteBadgeModal 
+                                isOpen={props.modalIsOpen} 
+                                onClose={props.onCloseModal}
+                                onDeleteBadge={props.onDeleteBadge}
+                                >Lorem Ipsum </DeleteBadgeModal>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default BadgeDetails;
